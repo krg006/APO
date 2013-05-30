b884ef47d61439bcafff2ce6e00e9d3be8793218
Ken Gorab <roboguy12@gmail.com>
2013-05-29 22:11:33 -0400

    Styled some things (calendar nav buttons)

--- includes/header_logged_in.php
+++ includes/header_logged_in.php
@@ -11,24 +11,33 @@
     </div>
 </div>
 
-
-<div class="notif_bar">
-	<div style="width: 720px; margin: 0px auto;">
-		
-		<span class="nice_big" isclickable="true" style="float: left;">Notifications</span>
-		
-		<span class="nice_big" id="logout" isclickable="true" style="float: right;">Log out</span>
-		<span class="nice_big" isclickable="false" style="float: right;">Hi, <?php echo user_get_first_name($_SESSION['username']); ?>!</span>
-
-		
-
+<div>
+	<div class="notif_bar">
+		<div style="width: 720px; margin: 0px auto;">
+			
+			<span class="nice_big" id="notifs" isclickable="true" style="float: left;">Notifications</span>
+			
+			<span class="nice_big" id="logout" isclickable="true" style="float: right;">Log out</span>
+			<span class="nice_big" isclickable="false" style="float: right;">Hi, <?php echo user_get_first_name($_SESSION['username']); ?>!</span>
+	
+			
+	
+		</div>	
 	</div>
 	
+	<div style="width: 720px; margin: 0px auto;">
+		<div id="notifications" style="background: #da9d19; height: 300px; width: 240px; position: absolute; z-index: 9990; display: none;">
+		</div>
+	</div>
 	
 </div>
 
 <script type="text/javascript">
+
+	
+
 	$(document).ready(function() {
+		var showNotifs = false;
 		$('#logout').click(function() {
 			$.ajax({
 				url : '../pages/logout.php',
@@ -40,6 +49,19 @@
 					alert(errorThrown);
 				}
 			});
+		});
+		
+		$('#notifs').click(function() {
+			if (showNotifs == false) {
+				showNotifs = true;
+				$('#notifications').fadeIn(50);
+				$('#notifs').css("background", "#da9d19");
+			} else {
+				showNotifs = false;
+				$('#notifications').fadeOut(50);
+				$('#notifs').css("background", "none");
+			}
+			
 		});
 	});
 </script>
